18f2f937510402dfc0d76b759b441a96bfbffd5a
Barclay Sayer <barclay.sayer@gmail.com>
2018-10-20 10:31:14 +1100

    ListBoxMultiSelect made public

--- Controls/ListBoxMultiSelect.cs
+++ Controls/ListBoxMultiSelect.cs
@@ -32,48 +32,48 @@ namespace Controls
             }
             base.OnLostKeyboardFocus(e);
         }
+    }
 
-        class ListBoxMultiSelectItem : ListBoxItem
+    public class ListBoxMultiSelectItem : ListBoxItem
+    {
+        private bool _deferSelection = false;
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
-            private bool _deferSelection = false;
-
-            protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+            if (e.ClickCount == 1 && IsSelected)
             {
-                if (e.ClickCount == 1 && IsSelected)
-                {
-                    // the user may start a drag by clicking into selected items
-                    // delay destroying the selection to the Up event
-                    _deferSelection = true;
-                    e.Handled = true;
-                }
-                else
+                // the user may start a drag by clicking into selected items
+                // delay destroying the selection to the Up event
+                _deferSelection = true;
+                e.Handled = true;
+            }
+            else
+            {
+                base.OnMouseLeftButtonDown(e);
+            }
+        }
+
+        protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            if (_deferSelection)
+            {
+                try
                 {
                     base.OnMouseLeftButtonDown(e);
                 }
-            }
-
-            protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
-            {
-                if (_deferSelection)
+                finally
                 {
-                    try
-                    {
-                        base.OnMouseLeftButtonDown(e);
-                    }
-                    finally
-                    {
-                        _deferSelection = false;
-                    }
+                    _deferSelection = false;
                 }
-                base.OnMouseLeftButtonUp(e);
             }
+            base.OnMouseLeftButtonUp(e);
+        }
 
-            protected override void OnMouseLeave(MouseEventArgs e)
-            {
-                // abort deferred Down
-                _deferSelection = false;
-                base.OnMouseLeave(e);
-            }
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            // abort deferred Down
+            _deferSelection = false;
+            base.OnMouseLeave(e);
         }
     }
 }
